394ff5114abfe98153a292be5a33c6c243aade2b
Keshav Sharma <keshav2743@gmail.com>
2022-12-07 11:52:38 +0530

    added memo and callback function

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import Button from './Components/UI/Button/Button'
 import DemoOutput from './Components/Demo/DemoOutput'
-import React, {useState} from 'react';
+import React, {useState, useCallback } from 'react';
+import './App.css'
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
   console.log('App running');
 
-  const toggleParagraphHandler = ()=> {
+
+  //if callback is not used every time app is rendered the toggleparagraphhandler will be treated as a new function
+  // using callback will store the function in the same memory
+  // for using callback you should use React.memo again
+
+  const toggleParagraphHandler = useCallback(()=> {
     setShowParagraph(prev => !prev)
-  }
+  },[])
 
   return (
     <div className="App">
       <h1>Hi there</h1>
-      <DemoOutput show={showParagraph} />
+      <DemoOutput show={false} />
      <Button onClick={toggleParagraphHandler}> Toggle Paragraph</Button>
     </div>
   );
--- src/Components/Demo/DemoOutput.jsx
+++ src/Components/Demo/DemoOutput.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const DemoOutput = (props) => {
+    console.log('inside demoApp')
     return <p>{props.show?'this is new': ''}</p>
 
 }
-export default DemoOutput;
+
+//it will do comparison with previous props value with the current props value
+// it will affect performance 
+export default React.memo(DemoOutput);
